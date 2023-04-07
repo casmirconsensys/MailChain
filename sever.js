bafaf44e733b4f87a67b22eb62520007ca00a29f
Casmir Patterson <casmirpatterson@Casmirs-MacBook-Pro-2.local>
2023-04-07 13:46:38 -0500

    added Authenticated Server

--- sever.js
+++ sever.js
@@ -1,20 +1,42 @@
 import { Mailchain } from '@mailchain/sdk';
+import App from '@/app';
+import IndexRoute from '@/routes/index.route';
+import MailRoute from '@/routes/mail.route';
 
-const seed = process.env.SECRET_RECOVERY_PHRASE; // hex encoded seed bytes
+const app = new App([new IndexRoute(), new AuthenticatedMailRoute()]);
+app.listen();
+
+
+const secretRecoveryPhrase = process.env.SECRET_RECOVERY_PHRASE; 
 const mailchain = Mailchain.fromSecretRecoveryPhrase(secretRecoveryPhrase);
+//SEDD
+cont seed = mailchain.env.SEED!; // hex encoded seed bytes
+const mailchain = Mailchain.fromAccountSeed(seed);
 
+// Path: mailchain/sever.js
 async function mail() {
+    //Verify the user
 const user = await mailchain.user();
 console.log(`username: ${user.username}, address: ${user.address}`);
 
-const result = await mailchain.send({
+//The code below sends a mail.
+const {data, error } = await mailchain.sendMail({
     from: 'askcasmir@ens.mailchain.com',
     to: ['0x5C0b2E97109a6aee0F40D63B5d70F6e9DD137240@ethereum.mailchain.com'],
-    subject: 'Hello World',
+    subject: 'Hello Mailchain',
     content: {
-        text: 'Hello World',
-        html: '<h1>Hello World</h1>',
+        text: 'Hello Mailchain',
+        html: '<h1>Hello Mailchain</h1>',
     },
-    })
-console.log(result);
+    });
+    if (error) {
+        // Handle error
+        console.error('Mailchain error', error);
+        return;
+    }
+    // Handle success
+console.log(data);
 }
+
+
+    
